6048efb3f0793c4a1e8f01b4655bb24a20c4c2a5
hammertime1070 <mbkeith614@gmail.com>
2023-08-27 15:58:06 -0400

    Add onclick functions for inputs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Set up variables
 let resultValue = 10;
 let operatorValue = 0;
+let inputArray = []
 var activeOperation = add;
 
 // Set up functions to change the activeOperation to the correct operation
@@ -44,6 +45,11 @@ function evaluate() {
     updateResultValue()
 }
 
+function pushToArray(n) {
+    inputArray.push(n)
+}
+
+
 // Function to get operatorValue
 // Eventually will need to wait for a new button press
 // and set the value of operatorValue
@@ -64,7 +70,7 @@ const subtract = document.querySelector('#subtract')
 const multiply = document.querySelector('#multiply')
 const divide = document.querySelector('#divide')
 
-const equals = document.querySelector('#equals')
+const pressEquals = document.querySelector('#pressEquals')
 
 const zero = document.querySelector('#zero')
 const one = document.querySelector('#one')
@@ -76,3 +82,24 @@ const six = document.querySelector('#six')
 const seven = document.querySelector('#seven')
 const eight = document.querySelector('#eight')
 const nine = document.querySelector('#nine')
+
+// UI On Clicks
+
+add.onclick = () => setAdd()
+subtract.onclick = () => setSubtract()
+multiply.onclick = () => setMultiply()
+divide.onclick = () => setDivide()
+
+pressEquals.onclick = () => evaluate()
+
+zero.onclick = () => pushToArray(0)
+one.onclick = () => pushToArray(1)
+two.onclick = () => pushToArray(2)
+three.onclick = () => pushToArray(3)
+four.onclick = () => pushToArray(4)
+five.onclick = () => pushToArray(5)
+six.onclick = () => pushToArray(6)
+seven.onclick = () => pushToArray(7)
+eight.onclick = () => pushToArray(8)
+nine.onclick = () => pushToArray(9)
+
